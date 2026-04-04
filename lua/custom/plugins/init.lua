fd3acee553d624a5d2c632a63d9702cef5ea4e13
Amar Kulovac <kulovac@ualberta.ca>
2026-04-04 17:12:14 -0600

    Markdown viewer

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -25,4 +25,13 @@ return {
     },
     opts = {},
   },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  },
 }
